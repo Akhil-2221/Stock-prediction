2e1cc9cc517f62497c893721520286f9d773d9a7
nithin reddy <akhilreddy2782@gmail.com>
2026-03-19 23:02:32 +0530

    Initial commit

---
+++ stock-main/backend-drf/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .serializers import UserSerializers
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [AllowAny]
+
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    
+    def get(self,request):
+        respone = {
+            'status' : 'Request was done'
+        }
+    
+        return Response(respone)
